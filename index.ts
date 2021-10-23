183d08384aa8cedde96a7f85871784551e328e8a
Ivan Bunin <chernobelenkiy@gmail.com>
2021-10-23 23:10:03 +0300

    init

---
+++ index.ts
@@ -0,0 +1,27 @@
+import TelegramBot from 'node-telegram-bot-api';
+require('dotenv').config();
+
+//@ts-ignore
+const bot = new TelegramBot(process.env.TOKEN, {polling: true});
+
+// Matches "/echo [whatever]"
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+
+  const chatId = msg.chat.id;
+  const resp = match?.[1] || ''; // the captured "whatever"
+
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, resp);
+});
+
+// Listen for any kind of message. There are different kinds of
+// messages.
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+
+  // send a message to the chat acknowledging receipt of their message
+  bot.sendMessage(chatId, 'Received your message');
+});
