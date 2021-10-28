458d99d0317ef9a0f6be3040b1033aae3d1a2e09
chernobelenkiy <enkiy222@gmail.com>
2021-10-28 14:16:45 +0300

    remove payment

--- index.ts
+++ index.ts
@@ -5,34 +5,41 @@ require('dotenv').config();
 const bot = new TelegramBot(process.env.TOKEN, {polling: true});
 
 const PAYMENT_REPLY: InlineKeyboardButton[][] = [
-  [{ text: 'Оплатить взнос', callback_data: 'payment', pay: true }],
-  [{ text: 'Перейти в основной канал', url: 'https://t.me/secret_league' }]
+  [{ text: 'Я честно оплатил взнос!', callback_data: 'payment' }]
 ];
 
 bot.onText(/\/start/, async (msg) => {
   bot.sendMessage(msg.chat.id,
-    'Привет! Тебя приветствует бот Тайной Лиги Леди и Джентльментов.', {
+    'Привет! Тебя приветствует бот Тайной Лиги Леди и Джентльментов.' +
+    '\n\nЧтобы начать обучение, необходимо внести взнос в нашу Лигу. Этот взнос станет частью призового фонда по окончанию обучения.' +
+    '\n\nЕсли ты готов, то переводи 1000 рублей по номеру +79602985181 (Иван Б, Сбер / Тинькофф)', {
       parse_mode: 'HTML',
       reply_markup: {
         inline_keyboard: PAYMENT_REPLY,
       }
-    })
+    });
 });
 
 bot.on('callback_query', (query) => {
   const chatId = query?.message?.chat.id;
 
   if (chatId && query.data === 'payment') {
-    query?.message?.chat.id && bot.sendInvoice(
-      query.message.chat.id,
-      'Тайный взнос',
-      'Взнос в Тайную Лигу Леди и Джентльменов. Этот взнос станет частью призового фонда по окончанию обучения.',
-      'payment_received',
-      process.env.PAYMENT_TOKEN || 'PAYMENT_TOKEN',
-      '',
-      'RUB',
-      [{ label: '1000 RUB', amount: 100000}],
+    bot.sendMessage(chatId, 
+      `
+        Отлично! Теперь переходи в основной канал!
+        <a href="${process.env.INVITE_LINK}">Тайная Лига Леди и Джентльменов</a>
+      `, { parse_mode: 'HTML'}
     );
+    // query?.message?.chat.id && bot.sendInvoice(
+    //   query.message.chat.id,
+    //   'Тайный взнос',
+    //   'Взнос в Тайную Лигу Леди и Джентльменов. ',
+    //   'payment_received',
+    //   process.env.PAYMENT_TOKEN || 'PAYMENT_TOKEN',
+    //   '',
+    //   'RUB',
+    //   [{ label: '1000 RUB', amount: 100000}],
+    // );
   }
 });
 
