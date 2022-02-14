cc0762c2b24c54ff680bf17e63c50d9b58d89e4c
Ivan Bunin <chernobelenkiy@gmail.com>
2022-02-14 22:37:47 +0300

    fix

--- bots/secretLeague/index.ts
+++ bots/secretLeague/index.ts
@@ -7,25 +7,25 @@ const PAYMENT_REPLY: InlineKeyboardButton[][] = [
   [{ text: 'Я честно оплатил взнос!', callback_data: 'payment' }]
 ];
 
-// bot.onText(/\/start/, async (msg) => {
-//   bot.sendMessage(msg.chat.id,
-//     'Привет! Тебя приветствует бот Тайной Лиги Леди и Джентльментов.' +
-//     '\n\nЗапись на обучение закрыта!', {
-//       parse_mode: 'HTML'
-//     });
-// });
-
 bot.onText(/\/start/, async (msg) => {
   bot.sendMessage(msg.chat.id,
     'Привет! Тебя приветствует бот Тайной Лиги Леди и Джентльментов.' +
-    '\n\nЧтобы начать обучение, необходимо внести взнос в нашу Лигу. Этот взнос станет частью призового фонда по окончанию обучения.' +
-    '\n\nЕсли ты готов, то переводи 1000 рублей по номеру +79602985181 (Иван Б, Сбер / Тинькофф)', {
-      parse_mode: 'HTML',
-      reply_markup: {
-        inline_keyboard: PAYMENT_REPLY,
-      }
+    '\n\nЗапись на обучение закрыта!', {
+      parse_mode: 'HTML'
     });
 });
+
+// bot.onText(/\/start/, async (msg) => {
+//   bot.sendMessage(msg.chat.id,
+//     'Привет! Тебя приветствует бот Тайной Лиги Леди и Джентльментов.' +
+//     '\n\nЧтобы начать обучение, необходимо внести взнос в нашу Лигу. Этот взнос станет частью призового фонда по окончанию обучения.' +
+//     '\n\nЕсли ты готов, то переводи 1000 рублей по номеру +79602985181 (Иван Б, Сбер / Тинькофф)', {
+//       parse_mode: 'HTML',
+//       reply_markup: {
+//         inline_keyboard: PAYMENT_REPLY,
+//       }
+//     });
+// });
 
 bot.on('callback_query', (query) => {
   const chatId = query?.message?.chat.id;
