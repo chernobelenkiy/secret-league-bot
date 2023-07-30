910658212c46ceaec7088ae5b38f26d729a42659
Ivan Bunin <chernobelenkiy@gmail.com>
2023-07-30 21:37:38 +0300

    temp check

--- bots/enkiyBoy/index.ts
+++ bots/enkiyBoy/index.ts
@@ -12,18 +12,12 @@ bot.on('message', async (msg) => {
   const chatId = msg.chat.id;
   const chatType = msg.chat.type;
   const replyToMessageId = msg.message_id;
+  const userId = msg?.from?.id;
+  console.log('userId: ', userId);
   if (!message) return;
 
-  if (chatType === 'channel' || chatType === 'supergroup') {
-    // Call OpenAI API to generate a response
-    const response = await generate(message, chatType);
-    if (!response) return;
+  const response = await generate(message, chatType);
+  if (!response) return;
 
-    // Post the response as a comment to the message
-    await bot.sendMessage(chatId, response, { reply_to_message_id: replyToMessageId });
-  } else {
-    // The message was not sent in a channel or in the comments section
-    // Do nothing or handle the message differently
-    bot.sendMessage(chatId, 'Что нужно тебя, кожаный мешок?');
-  }
+  await bot.sendMessage(chatId, response, { reply_to_message_id: replyToMessageId });
 });
