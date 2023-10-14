f142632116432e7cbe77cd5f9032eca4cf355d09
Ivan Bunin <chernobelenkiy@gmail.com>
2023-10-14 18:14:07 +0300

    add mediation bot

---
+++ src/_bots/MediationBot/ctx.ts
@@ -0,0 +1,21 @@
+import TelegramBot from 'node-telegram-bot-api';
+import { TContext } from '../../types';
+import { PromptManager } from '../../controllers/prompt';
+import { CommandsManager } from '../../controllers/cmd';
+import { MediationSystemPrompt } from './controllers/systemPrompt';
+import { createUserAccess } from './helpers';
+
+export const createContext = (msg: TelegramBot.Message, botInfo: TelegramBot.User, bot: TelegramBot): TContext => ({
+  data: {
+    chatId: msg.chat.id,
+    fromId: msg.from?.id,
+    text: msg.text || ''
+  },
+  prompts: [],
+  userAccess: createUserAccess(msg, botInfo),
+  systemPrompt: new MediationSystemPrompt(),
+  prompt: new PromptManager(),
+  bot,
+  cmd: new CommandsManager()
+});
+
