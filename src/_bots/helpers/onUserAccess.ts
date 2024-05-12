0df976d0153e7b1a57b16d1b2233f2149f996196
Ivan Bunin <chernobelenkiy@gmail.com>
2024-05-12 16:23:44 +0300

    logs

--- src/_bots/helpers/onUserAccess.ts
+++ src/_bots/helpers/onUserAccess.ts
@@ -28,6 +28,10 @@ export const onCreateUserAccess = async (
   const fromId = msg.reply_to_message?.from?.id;
   const isReplyToBot = fromId === botInfo?.id;
   const fromChannel = msg.from?.id?.toString() === "777000";
+  console.log("+++++++");
+  console.log(msg.from?.id?.toString());
+  console.log(msg.from?.id?.toString() === "777000");
+  console.log("_______");
   const fromWhitelistedUserInPrivate =
     chatType === "private" && whiteListIds.includes(userId as string);
 
